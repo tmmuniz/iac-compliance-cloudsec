914227f4dbf00d56d922eb13fc6390782fddc8a0
tmmuniz <tmmuniz92@gmail.com>
2026-04-24 16:36:17 -0300

    Remove public IP address from EC2

--- infra/modules/cloudsec-lab/main.tf
+++ infra/modules/cloudsec-lab/main.tf
@@ -269,7 +269,7 @@ resource "aws_instance" "app" {
   subnet_id                   = data.aws_subnets.default.ids[count.index]
   vpc_security_group_ids      = [aws_security_group.ec2.id]
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   user_data                   = local.user_data
 
   root_block_device {
