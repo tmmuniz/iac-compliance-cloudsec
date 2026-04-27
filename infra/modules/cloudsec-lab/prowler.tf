c4ac32a9cd5941c4f0c5c6785f7c8452c8ac9fb0
tmmuniz <tmmuniz92@gmail.com>
2026-04-27 20:37:27 +0000

    Remove role creation

--- infra/modules/cloudsec-lab/prowler.tf
+++ infra/modules/cloudsec-lab/prowler.tf
@@ -37,44 +37,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "prowler_reports" 
   }
 }
 
-resource "aws_iam_role" "github_actions_prowler_role" {
-  name = local.prowler_role_name
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
-          }
-        }
-      }
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = local.prowler_role_name
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "prowler_security_audit" {
-  role       = aws_iam_role.github_actions_prowler_role.name
-  policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
-}
-
-resource "aws_iam_role_policy_attachment" "prowler_view_only" {
-  role       = aws_iam_role.github_actions_prowler_role.name
-  policy_arn = "arn:aws:iam::aws:policy/job-function/ViewOnlyAccess"
-}
 
 resource "aws_iam_policy" "prowler_report_writer" {
   name = "prowler-report-writer"
